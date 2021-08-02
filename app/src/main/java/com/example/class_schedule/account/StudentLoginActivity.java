915beb8c610186048b0d17fb6dac3d92336fa692
loop <loopinfinity@criptext.com>
2021-08-02 20:56:21 +0300

    add account login feature ui

---
+++ app/src/main/java/com/example/class_schedule/account/StudentLoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.class_schedule.account;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.class_schedule.R;
+
+public class StudentLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_login);
+    }
+}
---
+++ app/src/main/java/com/example/class_schedule/account/TeacherLoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.class_schedule.account;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.class_schedule.R;
+
+public class TeacherLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_login);
+    }
+}
